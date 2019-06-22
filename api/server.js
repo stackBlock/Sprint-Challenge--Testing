2c4ab67bed9cc27109e40b2f044b5c158e48f646
anthony <stackchain@gmail.com>
2019-06-22 08:37:17 -0400

    commit - first post test passed

--- api/server.js
+++ api/server.js
@@ -10,10 +10,27 @@ server.use(express.json());
 
 
 server.get('/', (req, res) => {
-    res.status(200).json({ message: "Server is running" });
+    res
+        .status(200)
+        .json({ message: "Server is running" });
 })
 
+server.post('/games', (req, res) => {
+    const game = req.body;
 
+    if (!game.title || !game.genre)
+        res
+            .status(422)
+            .json({ message: "You got the wrong game" });
+    else {
+        games
+            .push(game);
+        res
+            .status(200)
+            .json(game);
+    }
+
+})
 
 
 module.exports = server;
